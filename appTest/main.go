8ecf5f67ef813d42836749a0ba520050684e098f
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-08-10 13:54:39 +0900

    success

--- appTest/main.go
+++ appTest/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	toolkit "github.com/cyrusmanosa/Toolkit/v2"
 )
 
 func main() {
@@ -19,6 +21,8 @@ func main() {
 func routes() http.Handler {
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
 	mux.HandleFunc("/api/login", login)
 	mux.HandleFunc("/api/logout", logout)
 
@@ -26,9 +30,37 @@ func routes() http.Handler {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
+	var tools toolkit.Tools
+	var payload struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
 
+	err := tools.ReadJSON(w, r, &payload)
+	if err != nil {
+		tools.ErrorJSON(w, err)
+		return
+	}
+
+	var resPayload toolkit.JSONResponse
+	if payload.Username == "me@here.com" && payload.Password == "verysecret" {
+		resPayload.Error = false
+		resPayload.Message = "Logged in"
+		tools.WriteJSON(w, http.StatusAccepted, resPayload)
+		return
+	}
+
+	resPayload.Error = true
+	resPayload.Message = "invalid credentials"
+	tools.WriteJSON(w, http.StatusAccepted, resPayload)
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
+	var tools toolkit.Tools
 
+	payload := toolkit.JSONResponse{
+		Message: "Logged out",
+	}
+
+	tools.WriteJSON(w, http.StatusAccepted, payload)
 }
