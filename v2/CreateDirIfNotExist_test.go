16a0c3e9eaa88b5768ac31ac4584ebbc16630990
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-10-20 14:44:39 +0900

    Change

---
+++ v2/CreateDirIfNotExist_test.go
@@ -0,0 +1,21 @@
+package toolkit
+
+import (
+	"os"
+	"testing"
+)
+
+func TestTools_CreateDirIfNotExist(t *testing.T) {
+	var testTools Tools
+
+	err := testTools.CreateDirIfNotExist("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+	err = testTools.CreateDirIfNotExist("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	os.Remove("./testdata/myDir")
+}
