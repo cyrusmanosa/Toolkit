e427f3626588a31d1b7758131e9cd1f3d0d4d9d8
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-12-26 14:50:34 +0900

    update

--- v2/Random.go
+++ v2/Random.go
@@ -2,15 +2,26 @@ package toolkit
 
 import "crypto/rand"
 
-const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+const randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
 
 // random characters
 func (t *Tools) RandomString(n int) string {
-	s, r := make([]rune, n), []rune(randomStringSource)
+	s, r := make([]rune, n), []rune(randomString)
 	for i := range s {
 		p, _ := rand.Prime(rand.Reader, len(r))
 		x, y := p.Uint64(), uint64(len(r))
 		s[i] = r[x%y]
 	}
 	return string(s)
+}
+
+func (t *Tools) RandomEmail() string {
+	s, r := make([]rune, 25), []rune(randomString)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s) + "@example@example.com"
 }
